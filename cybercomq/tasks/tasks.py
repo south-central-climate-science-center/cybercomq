ad743e5257e63e9810c3faa0096d362b9a0eb308
Duncan Wilson <dunc.wilson9@gmail.com>
2017-03-28 17:21:45 -0500

    blank update

---
+++ cybercomq/tasks/tasks.py
@@ -0,0 +1,19 @@
+from celery.task import task
+from dockertask import docker_task
+from subprocess import call,STDOUT
+import requests
+import os
+
+#Default base directory 
+basedir="/data/static/"
+
+
+#Example task
+@task()
+def add(x, y):
+    """ Example task that adds two numbers or strings
+        args: x and y
+        return addition or concatination of strings
+    """
+    result = x + y
+    return result

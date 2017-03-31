473a61774ebb2d7005d11b691b741a09dfe9baa7
Duncan Wilson <dunc.wilson9@gmail.com>
2017-03-31 14:04:11 -0500

    try

--- cybercomq/tasks/tasks.py
+++ cybercomq/tasks/tasks.py
@@ -19,18 +19,13 @@ def add(x, y):
     return result
 
 @task()
-def add_usingR(x, y):
+def add_usingR():
     task_id = str(add_usingR.request.id)
     resultDir = setup_result_directory(task_id)
-    host_data_resultDir = "/data/static/someapp_tasks/{0}".format(task_id)
-    runfile = "simple.R"	
-    #Run R Script in an R container
-    #docker_opts = "-d --rm -v '/opt/someapp/data/static':/home/$USER -w /home/$USER -e USERID=$UID "
-    docker_opts = '-w /home/dwilson1'	
-    docker_cmd ="Rscript simple.R"
+    docker_opts = '-v /opt/someapp/data/static:/script:z '	
+    docker_cmd ="Rscript /script/simple.R"
     result = docker_task(docker_name="rocker/r-base",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
-    #result_url ="http://{0}/someapp_tasks/{1}".format(result['host'],result['task_id'])
-    result_url = x + y
+    result_url ="http://{0}/someapp_tasks/{1}".format(result['host'],result['task_id'])
     return result_url
 
 #--volume /opt/someapp/data/static:/home/dwilson1 
